2a316485c838a31c4f56c52caea38fdf84d14627
Luis Lizarraga <siul.al4@gmail.com>
2015-11-10 10:32:26 -0600

    Agregando requirements

--- nexparser/settings.py
+++ nexparser/settings.py
@@ -77,13 +77,17 @@ WSGI_APPLICATION = 'nexparser.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'parser',
+    #     'HOST': 'localhost',
+    #     'USER': 'luis',
+    #     'PASSWORD': 'siulims',
+    #     'PORT': '',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'parser',
-        'HOST': 'localhost',
-        'USER': 'luis',
-        'PASSWORD': 'siulims',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     },
 }
 
